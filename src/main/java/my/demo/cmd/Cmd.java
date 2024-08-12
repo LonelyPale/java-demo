91348604d1cbb6b215c999ee8f565bd01fc4ca04
LonelyPale <lonelypale@126.com>
2024-08-12 14:40:49 +0800

    update notification

--- src/main/java/my/demo/cmd/Cmd.java
+++ src/main/java/my/demo/cmd/Cmd.java
@@ -6,16 +6,23 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class Cmd {
     public static void main(String[] args) throws IOException {
-        cmd2();
+        displayEnvironmentalInformation();
+//        cmd1();
     }
 
     public static void cmd1() throws IOException {
-        //        String command = String.format("/usr/local/bin/python -V");
-        String command = String.format("/usr/local/bin/bash -c \"python -V\"");
-        Process process = Runtime.getRuntime().exec(command);
+//        String currentDir = System.getenv().getProperty("user.dir");
+
+        //String command = "/usr/local/bin/python -V";
+        //String command = "bash --help";
+        //Process process = Runtime.getRuntime().exec(command);
+
+        String[] cmdarray = {"bash", "-c", ". ~/.profile; python -V"};
+        Process process = Runtime.getRuntime().exec(cmdarray);
 
         String line = null;
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -54,5 +61,27 @@ public class Cmd {
             System.out.println(line);
         }
         process.destroy();
+    }
+
+    public static void displayEnvironmentalInformation() {
+        System.out.println("===== ===== ===== env - start ===== ===== =====");
+        Map<String, String> env = System.getenv();
+        for (Map.Entry<String, String> entry : env.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.printf("%s=%s%n", key, value);
+        }
+        System.out.println("===== ===== ===== env - end   ===== ===== =====");
+
+        System.out.println();
+        System.out.println("===== ===== ===== properties - start ===== ===== =====");
+        Properties properties = System.getProperties();
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            Object key = entry.getKey();
+            Object value = entry.getValue();
+            System.out.printf("%s=%s%n", key, value);
+        }
+        System.out.println("===== ===== ===== properties - end   ===== ===== =====");
+
     }
 }
