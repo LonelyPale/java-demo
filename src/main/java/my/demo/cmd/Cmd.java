82f011d3b24cd59f2484282862fd7c1740380dd7
LonelyPale <lonelypale@126.com>
2024-08-12 09:57:04 +0800

    update notification

--- src/main/java/my/demo/cmd/Cmd.java
+++ src/main/java/my/demo/cmd/Cmd.java
@@ -5,10 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Cmd {
     public static void main(String[] args) throws IOException {
-        String command = String.format("/usr/local/bin/python -V");
+        cmd2();
+    }
+
+    public static void cmd1() throws IOException {
+        //        String command = String.format("/usr/local/bin/python -V");
+        String command = String.format("/usr/local/bin/bash -c \"python -V\"");
         Process process = Runtime.getRuntime().exec(command);
 
         String line = null;
@@ -24,6 +30,29 @@ public class Cmd {
             System.out.println(line);
         }
         process.destroy();
+    }
 
+    public static void cmd2() throws IOException {
+        String currentDir = System.getProperty("user.dir");
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("/usr/local/bin/python", "-V");
+        //processBuilder.command("/usr/local/bin/python -V");
+        Map<String, String> environment = processBuilder.environment();
+        environment.put("LD_LIBRARY_PATH", "/opt/occt762/lib");
+        Process process = processBuilder.start();
+
+        String line = null;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        // handle process error log and output
+        List<String> lines = new ArrayList<>();
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+            lines.add(line);
+        }
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        while ((line = reader1.readLine()) != null) {
+            System.out.println(line);
+        }
+        process.destroy();
     }
 }
